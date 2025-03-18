5f1894b4c689d6e5e71d92bd48fc3441e88723de
Magnus984 <nharnharyhaw984@gmail.com>
2025-03-18 11:03:09 +0000

    chore: setup project

---
+++ main.py
@@ -0,0 +1,41 @@
+"""Entry point for application"""
+from fastapi import FastAPI, status
+import uvicorn
+from api.v1.schemas.response_model import (
+                                SuccessResponse,
+                                StandardResponse
+                                )   
+
+app = FastAPI()
+
+@app.get("/",
+         tags=["Home"],
+         response_model=StandardResponse,
+         responses={
+             200: {
+                 "model": StandardResponse,
+                 "description": "Welcome response"
+             }
+         })
+async def get_root() -> dict:
+    """
+    Root endpoint for the API
+    
+    Returns:
+        Standardized success response with welcome message
+    """
+    success_response = SuccessResponse(
+        status_code=status.HTTP_200_OK,
+        message="Welcome to Base64-API",
+        data={}
+    )
+    return success_response
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=7001,
+        reload=True
+    )
